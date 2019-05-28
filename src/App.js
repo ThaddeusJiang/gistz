d8134b9273ab496b5bd3c39c4f5537d3490a2c9d
TJ Jiang <thaddeusjiang@gmail.com>
2019-05-28 23:31:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Remarkable from 'remarkable'
+import logo from './logo.svg'
+import './App.css'
+
+const GITHUB_API = `https://api.github.com/`
+const GIST_URL = `gists/`
 
 function App() {
+  const [postTitle, setTitle] = useState('')
+  const [updatedAt, setUpdatedAt] = useState('')
+  const [postDM, setContent] = useState('')
+
+  useEffect(() => {
+    async function fetchData() {
+      const { pathname } = document.location
+      const [, , gistId] = pathname.split('/')
+
+      const result = await fetch(`${GITHUB_API}${GIST_URL}${gistId}`).then(
+        (res) => res.json(),
+      )
+
+      const md = new Remarkable()
+
+      const { description, updated_at, files } = result
+      if (files[`index.md`]) {
+        const content = files[`index.md`].content
+        setTitle(description)
+        setUpdatedAt(updated_at)
+        setContent(md.render(content))
+      } else {
+        setTitle('not found!')
+      }
+    }
+
+    fetchData()
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{postTitle}</h1>
+      <p>{updatedAt}</p>
+      <div dangerouslySetInnerHTML={{ __html: postDM }} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
